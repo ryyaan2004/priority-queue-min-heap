47ac42074c6ad42f60311a2f79bc90854f469ec8
ryyaan2004 <ryyaan_2004@yahoo.com>
2016-02-27 15:20:55 -0600

    Implements min-heapify and
    Alters some access modifiers to package private for testing

--- src/main/java/org/ryyaan2004/datastructures/MinHeap.java
+++ src/main/java/org/ryyaan2004/datastructures/MinHeap.java
@@ -45,7 +45,7 @@ public class MinHeap {
 		return nodes[i];
 	}
 	
-	private int getHeight()
+	int getHeight()
 	{
 		return getLg(nodes.length);
 	}
@@ -55,13 +55,65 @@ public class MinHeap {
 	 * integer logarithms but it should be good enough for this
 	 * purpose I hope
 	 */
-	private int getLg(int a)
+	int getLg(int a)
 	{
 		return (int)( Math.log(a)/Math.log(2) );
 	}
 	
-	private int getCountOfLeaves()
+	int getCountOfLeaves()
 	{
 		return (int) Math.ceil( nodes.length / 2);
+	}
+	
+	void swap(int i, int j)
+	{
+		Node nodeI = nodes[i];
+		nodes[i] = nodes[j];
+		nodes[j] = nodeI;
+	}
+	
+	/**
+	 * Typically the signature of MIN-HEAPIFY would be
+	 * <code>MIN-HEAPIFY(A,i)</code> where A is the array
+	 * and i is the index postion from which to start. However
+	 * we're containing the array in this class and this class
+	 * should only be operating on it's own members
+	 * 
+	 * @param nodeIndex
+	 */
+	void minHeapify(int nodeIndex)
+	{
+		int leftChildIndex = getLeftChild(nodeIndex);
+		int rightChildIndex = getRightChild(nodeIndex);
+		int smallestIndex;
+		
+		/*
+		 * find the smallest, it should be at "top" 
+		 * or root of our 3 node "tree" 
+		 */
+		if ( leftChildIndex <= heapSize() &&
+				getNode(leftChildIndex).lessThan(getNode(nodeIndex)) )
+		{
+			smallestIndex = leftChildIndex;
+		}
+		else
+		{
+			smallestIndex = nodeIndex;
+		}
+		
+		if ( rightChildIndex <= heapSize() &&
+				getNode(rightChildIndex).lessThan(getNode(nodeIndex)) )
+		{
+			smallestIndex = rightChildIndex;
+		}
+		
+		/*
+		 * make our swap and a recursive call to ensure
+		 */
+		if ( smallestIndex != nodeIndex )
+		{
+			swap(nodeIndex, smallestIndex);
+			minHeapify(smallestIndex);
+		}
 	}
 }
